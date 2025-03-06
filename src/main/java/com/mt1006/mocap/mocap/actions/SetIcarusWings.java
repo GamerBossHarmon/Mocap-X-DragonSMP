18798da2c6fd19fa912f3098ad63da6b93f2ead9
GamerBossHarmon <93500405+GamerBossHarmon@users.noreply.github.com>
2025-03-06 00:36:15 -0700

    More testing

--- src/main/java/com/mt1006/mocap/mocap/actions/SetIcarusWings.java
+++ src/main/java/com/mt1006/mocap/mocap/actions/SetIcarusWings.java
@@ -1,16 +1,12 @@
 package com.mt1006.mocap.mocap.actions;
 
-import com.google.gson.Gson;
 import com.mt1006.mocap.mocap.files.RecordingFiles;
 import com.mt1006.mocap.mocap.playing.PlayingContext;
-import dev.emi.trinkets.api.SlotReference;
-import dev.emi.trinkets.api.TrinketComponent;
-import dev.emi.trinkets.api.TrinketsApi;
+import dev.emi.trinkets.api.*;
 import net.minecraft.core.Registry;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
@@ -19,15 +15,14 @@ import java.util.Optional;
 
 public class SetIcarusWings implements ComparableAction {
 
-    private static final Gson gson = new Gson();
-
+    //private final boolean hasWings;
     private final Item wings;
 
     private ItemStack temp;
 
     public SetIcarusWings(Entity entity) {
-        if (entity instanceof LivingEntity) {
-            Optional<TrinketComponent> trinketComponent = TrinketsApi.getTrinketComponent((LivingEntity) entity);
+        if (entity instanceof ServerPlayer) {
+            Optional<TrinketComponent> trinketComponent = TrinketsApi.getTrinketComponent((ServerPlayer) entity);
             trinketComponent.ifPresent(component -> {
                 component.getAllEquipped().forEach(tuple -> {
                     SlotReference slotReference = tuple.getA();
@@ -42,9 +37,14 @@ public class SetIcarusWings implements ComparableAction {
         }
         if (temp == null || temp.isEmpty()) {
             //System.out.println("No Wings Found");
+            //this.hasWings = false;
             this.wings = null;
         } else {
+            //this.hasWings = true;
             this.wings = temp.getItem();
+            TrinketComponent comp = TrinketsApi.getTrinketComponent((ServerPlayer) entity).get();
+            //SlotType slotNumber = TrinketsApi.getTrinketComponent((ServerPlayer) entity).get().getInventory().get("chest").get("cape").getSlotType();
+            //System.out.println("Slot Number: " + slotNumber);
         }
 
 
@@ -61,7 +61,6 @@ public class SetIcarusWings implements ComparableAction {
     }
 
     public SetIcarusWings(RecordingFiles.Reader reader) {
-        //this.wings = deserialize(reader.readString());
         //System.out.println("SetIcarusWings-:\n" + Registry.ITEM.get(ResourceLocation.tryParse(reader.readString())));
         this.wings = Registry.ITEM.get(ResourceLocation.tryParse(reader.readString()));
     }
@@ -84,14 +83,29 @@ public class SetIcarusWings implements ComparableAction {
 
     @Override
     public Result execute(PlayingContext ctx) {
-        if (!(ctx.entity instanceof LivingEntity)) {
+        if (!(ctx.entity instanceof ServerPlayer entity)) {
             return Result.IGNORED;
         }
 
         System.out.println("Execute: " + wings);
 
-        /*LivingEntity entity = (LivingEntity) ctx.entity;
-        Optional<TrinketComponent> trinketComponent = TrinketsApi.getTrinketComponent(entity);
+        //System.out.println("SetIcarusWings: " + Trinkets);
+
+        //TrinketComponent comp = TrinketsApi.getTrinketComponent((ServerPlayer) entity).get();
+        //SlotGroup slotGroup = comp.getGroups().getOrDefault(group, null);
+
+        //boolean canPlace = comp.getInventory().get("chest").get("cape").canPlaceItem(0, new ItemStack(wings));
+
+        //comp.getInventory().get("chest").get("cape").setStack(0, stack.createStack(amount, true));
+        //comp.getInventory().get("chest").get("cape").setItem(1, new ItemStack(wings));//46 is the slot number for a real player
+
+        //TrinketsApi.getTrinketComponent((ServerPlayer) entity).get().getInventory().get("chest").get("cape").setItem(0, new ItemStack(wings));
+        //System.out.println("Slot Number: " + TrinketsApi.getTrinketComponent(entity).get().getInventory().get("chest").get("cape").getSlotType(););
+        //entity.getInventory().setItem(0, new ItemStack(wings));
+
+        //entity.getInventory().setItem(46, new ItemStack(wings));
+
+        /*Optional<TrinketComponent> trinketComponent = TrinketsApi.getTrinketComponent(entity);
         if (trinketComponent.isPresent()) {
             TrinketComponent component = trinketComponent.get();
             component.getAllEquipped().forEach(tuple -> {
