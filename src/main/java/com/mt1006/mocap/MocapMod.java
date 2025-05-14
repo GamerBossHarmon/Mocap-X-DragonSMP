0bc71b2ed1fe45f126d3a3f35c94c79919ddf00e
GamerBossHarmon <93500405+GamerBossHarmon@users.noreply.github.com>
2025-05-14 04:18:50 -0600

    Bump to 0.1.0

--- src/main/java/com/mt1006/mocap/MocapMod.java
+++ src/main/java/com/mt1006/mocap/MocapMod.java
@@ -20,7 +20,7 @@ import org.apache.logging.log4j.Logger;
 public class MocapMod implements ModInitializer
 {
 	public static final String MOD_ID = "mocap";
-	public static final String VERSION = "1.3.9x-0.0.5";
+	public static final String VERSION = "1.3.9x-0.1.0";
 	public static final String FOR_VERSION = "1.19";
 	public static final String FOR_LOADER = "Fabric";
 	public static final Logger LOGGER = LogManager.getLogger();
